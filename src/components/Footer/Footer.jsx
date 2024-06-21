6b40ac8036390f1aa23794a821c3197b622c4765
Mammen Mathew <mammen999@gmail.com>
2024-06-21 21:50:27 +0530

    footer completed react router installed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './Footer.css'
+import youtube_icon from '../../assets/youtube_icon.png'
+import twitter_icon from '../../assets/twitter_icon.png'
+import instagram_icon from '../../assets/instagram_icon.png'
+import facebook_icon from '../../assets/facebook_icon.png'
+
 
 const Footer = () => {
   return (
-    <div className='footer'>Footer</div>
+    <div className='footer'>
+      <div className="footer-icons">
+        <img src={youtube_icon} alt="" srcset="" />
+        <img src={twitter_icon} alt="" srcset="" />
+        <img src={instagram_icon} alt="" srcset="" />
+        <img src={facebook_icon } alt="" srcset="" />
+      </div>
+      <ul>
+        <li>Audio Descripition</li>
+        <li>Help center</li>
+        <li>jobs</li>
+        <li>Media center</li>
+        <li>Terms of us </li>
+        <li>Media center</li>
+        <li>Media center</li>
+      </ul>
+      <p className='copyright-text'>© 1999-2024 mNetflix.inc</p>
+    </div>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png' 
 import TitleCards from '../../components/TitleCards/TitleCards'
+import Footer from '../../components/Footer/Footer'
 
 
 
@@ -31,9 +32,8 @@ function Home (){
     <TitleCards title={'Only on Netflix'}/>
     <TitleCards title={'Upcomming'}/>
     <TitleCards title={'Topics for you'}/>
-
-
     </div>
+    <Footer/>
     </>
   )
 }
