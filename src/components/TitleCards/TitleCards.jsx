12b2bc5eebf0a2754712710b104c93ee3cf9a2a3
Mammen Mathew <mammen999@gmail.com>
2024-06-21 21:14:40 +0530

    scroll without useEffect

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = () => {
+  const cardsRef = useRef();
+
+  const handleWheel = (e) => {
+    e.preventDefault();
+    cardsRef.current.scrollLeft += e.deltaY;
+  }
   return (
-    <div className='titlecards'>TitleCards</div>
+    <div className='title-cards'>
+      <h2>Popular on Netflix</h2>
+      <div className="card-list" ref={cardsRef} onWheel={handleWheel}>
+        {
+          cards_data.map((cards,index)=>{
+           return <div className='cards' key={index}>
+            <img src={cards.image} alt="" srcset="" />
+            <p>{cards.name}</p> 
+           </div>
+          })
+        }
+
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png' 
+import TitleCards from '../../components/TitleCards/TitleCards'
 
 
 
@@ -21,8 +22,8 @@ function Home (){
         <div className="hero-btns">
           <button className='btn'><img src={play_icon}></img>Play</button>
           <button className='btn dark-btn'><img src={info_icon}></img>More info</button>
-
         </div>
+        <TitleCards/>
       </div>
     </div>
     </>
