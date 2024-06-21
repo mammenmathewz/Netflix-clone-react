299d0dc4f9f9356043b9ccd912b09f95e2bbdeaa
Mammen Mathew <mammen999@gmail.com>
2024-06-21 20:26:20 +0530

    home- banner static

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png' 
 
- function Home (){
+
+
+
+function Home (){
   return (
     <>
     <Navbar/>
+    <div className="hero">
+      <img src={hero_banner} className='banner-img'></img>
+      <div className="hero-caption">
+        <img src={hero_title} alt="" srcset="" className='caption-img'/>
+        <p>gsukyhsdfkj usifda huiswafi hiufswaiauwsefiwoahjklds aiudsayf sd</p>
+        <div className="hero-btns">
+          <button className='btn'><img src={play_icon}></img>Play</button>
+          <button className='btn dark-btn'><img src={info_icon}></img>More info</button>
+
+        </div>
+      </div>
+    </div>
     </>
   )
 }
